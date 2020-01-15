f93b66a93298b5d894bca2d4ecb30890185affd0
robomantis19 <freefavors@gmail.com>
2020-01-15 17:10:00 -0500

    added userRouter up till delete

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js')
 
 const server = express();
+server.use(express.json()); 
+
+function logger(req, res, next) {
+  const { method, originalUrl } = req; 
+  console.log(`${method} to ${originalUrl}`);
+  next();
+}
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+
 });
+server.use(logger);
+server.use('/api/users', userRouter); 
 
 //custom middleware
 
-function logger(req, res, next) {}
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,25 +1,55 @@
 const express = require('express');
-
+const Users = require('./userDb.js'); 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-});
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
+    const Info = {...req.body, id: req.params.id}
+    Users.insert(Info)
+    .then(users2 => { 
+      res.status(200).json(users2);
+
+    })
+    .catch(err => { 
+      console.log(err); 
+      res.status(500).json({errorMessage: "error in post request"}); 
+    })
+
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get(req.query)
+  .then(users => { 
+    res.status(200).json(users);
+  })
+  .catch(err => { 
+    res.status(500).json({message: "error in get request"})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(users => { 
+    res.status(200).json(users); 
+  })
+  .catch(err => { 
+    res.status(500).json({message: "error in get by Id"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  Users.getUserPosts(req.params.id)
+  .then(users => { 
+    res.status(200).json(users)
+  })
+  .catch(err => { 
+    console.log('get by id posts error: ', err); 
+    res.status(500).json({message: "get posts by id error"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
